bab5d1c39f36d29178424b9bdb2299582941da6c
Muhydeen Adediran <50783764+adediranmuhydeen@users.noreply.github.com>
2023-01-24 22:54:31 +0100

    HMS-104

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import ResetUpdatePassword from "../src/components/ResetUpdatePassword/ResetUpda
 import ResetPassword from "./ResetPassword";
 import ChangePassword from "./ChangePassword";
 //import './App.css';
-import Sidebar from "./components/navs/Sidebar";
+//import Sidebar from "./components/navs/Sidebar";
+import Create from "./components/modals/CreatedModal";
+import Delete from "./components/modals/DeleteModal";
+import Update from "./components/modals/UpdatedModal";
+import Error from "./components/modals/ErrorModal";
+import Success from "./components/modals/SuccessModal";
 
 function App() {
   return (
@@ -17,6 +22,11 @@ function App() {
         <Route path="reset-update-password" element={<ResetUpdatePassword />} />
         <Route path="reset-password" element={<ResetPassword />} />
         <Route path="change-password" element={<ChangePassword />} />
+        <Route path="create-modal" element={<Create />} />
+        <Route path="delete-modal" element={<Delete />} />
+        <Route path="update-modal" element={<Update />} />
+        <Route path="error-modal" element={<Error />} />
+        <Route path="success-modal" element={<Success />} />
       </Routes>
     </BrowserRouter>
   );
