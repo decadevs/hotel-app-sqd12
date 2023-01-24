066ce5da656f12b6a1a0ce91116a6d7b68fadaa4
TCipher <tochind29@gmail.com>
2023-01-24 03:53:41 +0100

    ResetPassword

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ErrorBoundary from './components/common/ErrorBoundary';
+import ResetPassword from './ResetPassword';
+import ChangePassword from './ChangePassword';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ErrorBoundary>
       <Suspense fallback= "loading">
-        <App/>
+        {/* <App/> */}
+        {/* <ResetPassword/> */}
+        <ChangePassword/>
       </Suspense>
     </ErrorBoundary>
   </React.StrictMode>
