e145e0bb6e4de94a4b7362f78ec36abc4731b476
Muhydeen Adediran <50783764+adediranmuhydeen@users.noreply.github.com>
2023-01-25 16:48:02 +0100

    Modals

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+//import "./App.css";
 import Login from "./components/authentication/Login";
  import Register from "./components/authentication/Register";
 import ResetUpdatePassword from "../src/components/ResetUpdatePassword/ResetUpdatePassword";
@@ -12,10 +12,6 @@ import './components/PayForHotel/PayForHotelForm.css';
 import {PayForHotelForm} from './components/PayForHotel/PayForHotelForm';
 import LandingPage from"./components/landingPage/landingPageIndex/LandingPage";
 import Transactions from "./components/customerHome/Transactions/Transactions";
-
- 
-import ResetPassword from "./ResetPassword";
-import ChangePassword from "./ChangePassword";
 import Create from "./components/modals/CreatedModal";
 import Delete from "./components/modals/DeleteModal";
 import Update from "./components/modals/UpdatedModal";
@@ -33,8 +29,8 @@ function App() {
         <Route path="reset-password" element={<ResetPassword />} />
         <Route path="change-password" element={<ChangePassword />} />
         <Route path="user-transaction" element={< UserTransactions/>} />
-         <Route path="customer-transactions" element={<Transactions />}/> 
-      <Route path="pay-for-hotel" element={<PayForHotelForm />} /> 
+        <Route path="customer-transactions" element={<Transactions />}/> 
+        <Route path="pay-for-hotel" element={<PayForHotelForm />} /> 
         <Route path="addroom" element={<AddRoom />} />
         <Route path="create-modal" element={<Create />} />
         <Route path="delete-modal" element={<Delete />} />
