f539a77b7eb2d47e683150e715bd3418a5bb0893
Ekene Obiasogu <ekennyobiasogu@gmail.com>
2023-01-26 00:26:51 +0100

    dashboard

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/authentication/Login";
  import Register from "./components/authentication/Register";
 import ResetUpdatePassword from "../src/components/ResetUpdatePassword/ResetUpdatePassword";
-<<<<<<< HEAD
 import ResetPassword from "./ResetPassword";
 import ChangePassword from "./ChangePassword";
 import CustomerDashboard from "./components/customerHome/Dashboard";
@@ -12,9 +11,6 @@ import AdminDashboard from "./components/adminHome/Dashboard";
 import CustomerHotel from "./components/customerHome/Hotels";
 import CustomerManager from "./components/customerHome/Manager";
 import CustomerTransaction from "./components/customerHome/Transactions";
-
-
-=======
 import AddRoom from "../src/pages/manager/AddRoom";
 import Sidebar from "./components/navs/Sidebar";
 import UserTransactions from "./components/customerHome/usertransaction/UserTransactions";
@@ -29,7 +25,6 @@ import Delete from "./components/modals/DeleteModal";
 import Update from "./components/modals/UpdatedModal";
 import Error from "./components/modals/ErrorModal";
 import Success from "./components/modals/SuccessModal";
->>>>>>> 939f7ff9898ff57d8f17ad538a456919b0bf70ef
 
 function App() {
   return (
@@ -41,14 +36,12 @@ function App() {
         <Route path="reset-update-password" element={<ResetUpdatePassword />} />
         <Route path="reset-password" element={<ResetPassword />} />
         <Route path="change-password" element={<ChangePassword />} />
-<<<<<<< HEAD
         <Route path="customer-dashboard" element={<CustomerDashboard />} />
         <Route path="manager-dashboard" element={<ManagerDashboard />} />
         <Route path="admin-dashboard" element={<AdminDashboard />} />
         <Route path="customer-hotel" element={<CustomerHotel/>} />
         <Route path="customer-manager" element={<CustomerManager/>} />
         <Route path="customer-transaction" element={<CustomerTransaction/>} />
-=======
         <Route path="user-transaction" element={< UserTransactions/>} />
         <Route path="customer-transactions" element={<Transactions />}/> 
         <Route path="pay-for-hotel" element={<PayForHotelForm />} /> 
@@ -58,7 +51,6 @@ function App() {
         <Route path="update-modal" element={<Update />} />
         <Route path="error-modal" element={<Error />} />
         <Route path="success-modal" element={<Success />} />
->>>>>>> 939f7ff9898ff57d8f17ad538a456919b0bf70ef
       </Routes>
       
       
