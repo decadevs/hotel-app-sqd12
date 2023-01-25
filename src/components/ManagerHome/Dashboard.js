3fdfb1d8ca03f50117a9f81e43f1945489384dc5
aristokratos <oluwatobiloba.onawale@decagon.dev>
2023-01-25 13:16:43 +0100

    stlying

--- src/components/ManagerHome/Dashboard.js
+++ src/components/ManagerHome/Dashboard.js
@@ -37,26 +37,26 @@ function Dashboard() {
           <div className="address">Address: 23 Ohen Street, Edo</div>
         </div>
       </div>
-            <div className="tranTable">
-    <div className="TransactionSearchBar">
+            <div className="managerTable">
+    <div className="ManagerSearchBar">
     <span>
-        <BiFilter className="TransactionFilterIcon" />
+        <BiFilter className="ManagerFilterIcon" />
     </span>
-        <div className="TransactionsearchInput">
-    <HiMagnifyingGlass className="TransactionSearchIcon" />
+        <div className="ManagersearchInput">
+    <HiMagnifyingGlass className="ManagerSearchIcon" />
     <input type="text" placeholder="Search" />
        </div><button class="add-customer new-style">
   <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M10 0C4.48 0 0 4.48 0 10C0 15.52 4.48 20 10 20C15.52 20 20 15.52 20 10C20 4.48 15.52 0 10 0ZM7 13H13V7H7V13ZM13 13C13 13.55 12.55 14 12 14C11.45 14 11 13.55 11 13V7C11 6.45 11.45 6 12 6C12.55 6 13 6.45 13 7V13Z" fill="#fff"/>
   </svg>
-  
+
   Add customer
 </button>
 
 </div>
     <table>
       <tr>
-        <th className="TranCheckBox">
+        <th className="ManagerCheckBox">
         <input type="checkbox" />
         </th>
             <th>MANAGER NAME</th>
@@ -68,14 +68,14 @@ function Dashboard() {
        </th>
       </tr>
     <tr>
-          <td className="TranCheckBox">
+          <td className="ManagerCheckBox">
                 <input type="checkbox" />
           </td>
                 <td>Kadin Herwitz</td>
                 <td>Lorem Ipsumdolor sit</td>
                 <td>66666666</td>
             <td>
-          <div className="TransactionCrud">
+          <div className="ManagerCrud">
                 <span>
                       <MdModeEdit />
                 </span>
@@ -89,13 +89,13 @@ function Dashboard() {
             </td>
             </tr>
             <tr>
-            <td className="TranCheckBox">
+            <td className="ManagerCheckBox">
             <input type="checkbox" />
             </td>
             <td>Kadin Herwitz</td>
             <td>Lorem Ipsumdolor sit</td>
             <td>778777777</td>
-            <div className="TransactionCrud">
+            <div className="ManagerCrud">
             <span>
             <MdModeEdit />
             </span>
@@ -108,13 +108,13 @@ function Dashboard() {
             </div>
             </tr>
             <tr>
-            <td className="TranCheckBox">
+            <td className="ManagerCheckBox">
             <input type="checkbox" />
             </td>
             <td>Kadin Herwitz</td>
             <td>Lorem Ipsumdolor sit</td>
             <td>66666666</td>
-            <div className="TransactionCrud">
+            <div className="ManagerCrud">
             <span>
             <MdModeEdit />
             </span>
@@ -127,13 +127,13 @@ function Dashboard() {
             </div>
             </tr>
             <tr>
-            <td className="TranCheckBox">
+            <td className="ManagerCheckBox">
             <input type="checkbox" />
             </td>
             <td>Kadin Herwitz</td>
             <td>Lorem Ipsumdolor sit</td>
             <td>778777777</td>
-            <div className="TransactionCrud">
+            <div className="ManagerCrud">
             <span>
             <MdModeEdit />
             </span>
@@ -146,13 +146,13 @@ function Dashboard() {
             </div>
             </tr>
             <tr>
-            <td className="TranCheckBox">
+            <td className="ManagerCheckBox">
             <input type="checkbox" />
             </td>
             <td>Kadin Herwitz</td>
             <td>Lorem Ipsumdolor sit</td>
             <td>778777777</td>
-            <div className="TransactionCrud">
+            <div className="ManagerCrud">
             <span>
             <MdModeEdit />
             </span>
@@ -165,13 +165,13 @@ function Dashboard() {
             </div>
             </tr>
             <tr>
-            <td className="TranCheckBox">
+            <td className="ManagerCheckBox">
             <input type="checkbox" />
             </td>
             <td>Kadin Herwitz</td>
             <td>Lorem Ipsumdolor sit</td>
             <td>778777777</td>
-            <div className="TransactionCrud">
+            <div className="ManagerCrud">
             <span>
             <MdModeEdit />
             </span>
@@ -184,13 +184,13 @@ function Dashboard() {
             </div>
             </tr>
             <tr>
-            <td className="TranCheckBox">
+            <td className="ManagerCheckBox">
             <input type="checkbox" />
             </td>
             <td>Kadin Herwitz</td>
             <td>Lorem Ipsumdolor sit</td>
             <td>778777777</td>
-            <div className="TransactionCrud">
+            <div className="ManagerCrud">
             <span>
             <MdModeEdit />
             </span>
@@ -203,13 +203,13 @@ function Dashboard() {
             </div>
             </tr>
             <tr>
-            <td className="TranCheckBox">
+            <td className="ManagerCheckBox">
             <input type="checkbox" />
             </td>
             <td>Kadin Herwitz</td>
             <td>Lorem Ipsumdolor sit</td>
             <td>778777777</td>
-            <div className="TransactionCrud">
+            <div className="ManagerCrud">
             <span>
             <MdModeEdit />
             </span>
@@ -222,13 +222,13 @@ function Dashboard() {
             </div>
             </tr>
             <tr>
-            <td className="TranCheckBox">
+            <td className="ManagerCheckBox">
             <input type="checkbox" />
             </td>
             <td>Kadin Herwitz</td>
             <td>Lorem Ipsumdolor sit</td>
             <td>778777777</td>
-              <div className="TransactionCrud">
+              <div className="ManagerCrud">
               <span>
                   <MdModeEdit />
               </span>
@@ -241,13 +241,13 @@ function Dashboard() {
             </div>
             </tr>
             <tr>
-            <td className="TranCheckBox">
+            <td className="ManagerCheckBox">
             <input type="checkbox" />
             </td>
             <td>Kadin Herwitz</td>
             <td>Lorem Ipsumdolor sit</td>
             <td>66666666</td>
-            <div className="TransactionCrud">
+            <div className="ManagerCrud">
             <span>
             <MdModeEdit />
             </span>
@@ -260,22 +260,22 @@ function Dashboard() {
             </div>
             </tr>
             </table>
-            <div className="Transaction-pagination">
+            <div className="Manager-pagination">
               <div>
-                  <h10>ACTIVE MANAGERS: 470/706</h10>
+                  <h15>ACTIVE MANAGERS: 470/706</h15>
               </div>
               <div>
-                <label for="Transaction-dd">
+                <label for="Manager-dd">
                     <b>Rows per page</b>
                 </label>
-              <select id="Transaction-dd" name="number">
+              <select id="Manager-dd" name="number">
                   <option value="null"> </option>
                   <option value="five">5</option>
                   <option value="ten">10</option>
                   <option value="twenty">20</option>
               </select>
             <div>
-            <h4>1-10 of 706</h4>
+            <h10>1-10 of 706</h10>
             <FaAngleLeft />
             <FaAngleRight />
             </div>
