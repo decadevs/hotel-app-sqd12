ec3ebb565d8b11d1bd2f1aa6966ccd0420369f5f
Dominic James <30402312+dom3691@users.noreply.github.com>
2023-01-25 01:52:24 +0100

    fixed errors

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
 import ResetUpdatePassword from './components/ResetUpdatePassword/ResetUpdatePassword'
-import ProfileLayout from './layouts/userProfileLayout/userProfileLayout'
+import {ProfileLayout} from './layouts/userProfileLayout/userProfileLayout'
 
 function App() {
   return (
