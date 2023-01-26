6ddebae0efe69204e702862611e9f31a2fee27ce
Okoro Nnamdi <okoronnamdi4044@gmail.com>
2023-01-26 10:47:11 +0100

    Review commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 //import "./App.css";
 import Login from "./components/authentication/Login";
- import Register from "./components/authentication/Register";
+import Register from "./components/authentication/Register";
 import ResetUpdatePassword from "../src/components/ResetUpdatePassword/ResetUpdatePassword";
 import AddRoom from "../src/pages/manager/AddRoom";
 import Sidebar from "./components/navs/Sidebar";
@@ -9,8 +9,8 @@ import UserTransactions from "./components/customerHome/usertransaction/UserTran
 import ResetPassword from "./components/authentication/ResetPassword";
 import ChangePassword from "./components/authentication/ChangePassword";
 import './components/PayForHotel/PayForHotelForm.css';
-import {PayForHotelForm} from './components/PayForHotel/PayForHotelForm';
-import LandingPage from"./components/landingPage/landingPageIndex/LandingPage";
+import { PayForHotelForm } from './components/PayForHotel/PayForHotelForm';
+import LandingPage from "./components/landingPage/landingPageIndex/LandingPage";
 import Transactions from "./components/customerHome/Transactions/Transactions";
 import Create from "./components/modals/CreatedModal";
 import Delete from "./components/modals/DeleteModal";
@@ -19,21 +19,24 @@ import Error from "./components/modals/ErrorModal";
 import Success from "./components/modals/SuccessModal";
 import Booking from "./pages/customer/Booking";
 import ViewMore from "./components/viewMore/viewMore";
+import HotelReview from "./components/HotelReview/HotelReview";
 
-function App() {
+function App()
+{
   return (
     <BrowserRouter>
-    
+
       <Routes>
+        <Route path="hotel-review" element={<HotelReview />} />
         <Route path="register" element={<Register />} />
-        <Route path="login" element={<Login />} /> 
+        <Route path="login" element={<Login />} />
         <Route path="reset-update-password" element={<ResetUpdatePassword />} />
         <Route path="reset-password" element={<ResetPassword />} />
         <Route path="change-password" element={<ChangePassword />} />
         <Route path="booking" element={<Booking />} />
-        <Route path="user-transaction" element={< UserTransactions/>} />
-        <Route path="customer-transactions" element={<Transactions />}/> 
-        <Route path="pay-for-hotel" element={<PayForHotelForm />} /> 
+        <Route path="user-transaction" element={< UserTransactions />} />
+        <Route path="customer-transactions" element={<Transactions />} />
+        <Route path="pay-for-hotel" element={<PayForHotelForm />} />
         <Route path="addroom" element={<AddRoom />} />
         <Route path="create-modal" element={<Create />} />
         <Route path="delete-modal" element={<Delete />} />
@@ -42,8 +45,8 @@ function App() {
         <Route path="success-modal" element={<Success />} />
         <Route path="view-more" element={<ViewMore />} />
       </Routes>
-      
-      
+
+
     </BrowserRouter>
   );
 }
