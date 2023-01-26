6331dc943bd1e9f881caed631b63af7f1511cf20
Dominic James <30402312+dom3691@users.noreply.github.com>
2023-01-27 00:14:05 +0100

    Reolving conflict2

--- src/layouts/userProfileLayout/ProfileLayout.jsx
+++ src/layouts/userProfileLayout/ProfileLayout.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import DashboardIcon from '../../assets/dashboard-solid-24 1.svg'
 import ManagerIcon from '../../assets/dashboard-manager 24 3.svg'
@@ -11,47 +11,56 @@ import imageEllipse from '../../assets/imageEllipse 5.svg'
 
 
 export const ProfileLayout = () => {
+    const [isActive, setIsActive] = useState(false);
+
+    const handleClick = event => {
+        // 👇️ toggle isActive state on click
+        setIsActive(current => !current);
+      };
+
   return (
     <div className='pl_container'>
-        <div className='pl_sideBar' >
-            <div className='pl_iconGroup'>
-              <Link to="/dashboard">
-                 <div className='pl_iconContainer'>
-                    <img src={DashboardIcon} className="pl_Icons"/>
-                    <span>Dashboard</span>
-                </div>
-              </Link>
+        <div className="sideBar_wrapper">
 
-              <Link to="">
-                 <div className='pl_iconContainer'>
-                    <img src={Solid} className="pl_Icons"/>
-                    <span>Hotels</span>
-                </div>
-              </Link>
+            <div className='pl_sideBar' >
+                <div className='pl_iconGroup'>
+                <Link to="/ dashboard" >
+                    <div className='pl_iconContainer'>
+                        <img src={DashboardIcon} className="pl_Icons"/>
+                        <span>Dashboard</span>
+                    </div>
+                </Link>
 
-              <Link to="">
-                 <div className='pl_iconContainer'>
-                    <img src={ManagerIcon} className="pl_Icons"/>
-                    <span>Profile</span>
-                </div>
-              </Link>
+                <Link to="" >
+                    <div className='pl_iconContainer'>
+                        <img src={Solid} className="pl_Icons"/>
+                        <span>Hotels</span>
+                    </div>
+                </Link>
 
-              <Link to="">
-                 <div className='pl_iconContainer'>
-                    <img src={TransactionIcon} className="pl_Icons"/>
-                    <span>Transaction</span>
-                </div>
-              </Link>
+                <Link to="/profile" className={isActive ? 'active' : ''} onClick={handleClick}>
+                    <div className='pl_iconContainer'>
+                        <img src={ManagerIcon} className="pl_Icons"/>
+                        <span className={isActive ? 'active' : ''}>Profile</span>
+                    </div>
+                </Link>
 
-                
+                <Link to="" >
+                    <div className='pl_iconContainer'>
+                        <img src={TransactionIcon} className="pl_Icons"/>
+                        <span>Transaction</span>
+                    </div>
+                </Link>
+
+                    
+                </div>
+
+                <div className='pl_iconContainer'>
+                    <img src={logoutIcon} className="pl_Icons"/>
+                    <span>Logout</span>
+                </div>
+            
             </div>
-
-            <div className='pl_iconContainer'>
-                 <img src={logoutIcon} className="pl_Icons"/>
-                <span>Logout</span>
-            </div>
-           
-        
         </div>
 
         <div className='pl_formContainer'>
@@ -62,20 +71,20 @@ export const ProfileLayout = () => {
                     <img src={imageEllipse} className="pl_Icons profileImage"/>
 
                     <div className="pl_titleContainer"> 
-                            <h1>Manager</h1>
-                            <h2>Profile Name</h2>
+                            <h1>User</h1>
+                            <h2>Profile </h2>
                     </div>
 
                 </div>
 
                 <div className='pl_formNestedDiv'>
-                    <div className='pl_formInputContainer'><input type="text" placeholder='Full Name' className='pl_formInput'/><img src={penVector} className="pl_penImg"/></div>
-                    <div className='pl_formInputContainer'><input type="text" placeholder='Date of Birth' className='pl_formInput'/><img src={penVector} className="pl_penImg"/></div>
-                    <div className='pl_formInputContainer'><input type="text" placeholder='Phone Number' className='pl_formInput'/><img src={penVector} className="pl_penImg"/></div>
-                    <div className='pl_formInputContainer'><input type="text" placeholder='Email Address' className='pl_formInput'/><img src={penVector} className="pl_penImg"/></div>
-                    <div className='pl_formInputContainer'><input type="text" placeholder='Hotel Name' className='pl_formInput'/><img src={penVector} className="pl_penImg"/></div>
-                    <div className='pl_formInputContainer'><input type="text" placeholder='Hotel Primary Address' className='pl_formInput'/><img src={penVector} className="pl_penImg"/></div>
-                    <div className='pl_formInputContainer'><input type="text" placeholder='Hotel Contact Phone' className='pl_formInput'/><img src={penVector} className="pl_penImg"/></div>
+                    <div className='pl_formInputContainer'><input type="text" placeholder='Full Name' className='pl_formInput'/><span className='pen_ctner'><img src={penVector} className="pl_penImg"/></span></div>
+                    <div className='pl_formInputContainer'><input type="text" placeholder='Date of Birth' className='pl_formInput'/><span className='pen_ctner'><img src={penVector} className="pl_penImg"/></span></div>
+                    <div className='pl_formInputContainer'><input type="text" placeholder='Phone Number' className='pl_formInput'/><span className='pen_ctner'><img src={penVector} className="pl_penImg"/></span></div>
+                    <div className='pl_formInputContainer'><input type="text" placeholder='Email Address' className='pl_formInput'/><span className='pen_ctner'><img src={penVector} className="pl_penImg"/></span></div>
+                    <div className='pl_formInputContainer'><input type="text" placeholder='Hotel Name' className='pl_formInput'/><span className='pen_ctner'><img src={penVector} className="pl_penImg"/></span></div>
+                    <div className='pl_formInputContainer'><input type="text" placeholder='Hotel Primary Address' className='pl_formInput'/><span className='pen_ctner'><img src={penVector} className="pl_penImg"/></span></div>
+                    <div className='pl_formInputContainer'><input type="text" placeholder='Hotel Contact Phone' className='pl_formInput'/><span className='pen_ctner'><img src={penVector} className="pl_penImg"/></span></div>
                 </div>
             </form>
         </div>        
