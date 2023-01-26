7e049c93e5cb021491aa85a91f2550d38f9baee3
DORIS-DUNU <dunudoris3@gmail.com>
2023-01-26 11:07:17 +0100

    Hotel View

--- src/App.js
+++ src/App.js
@@ -25,23 +25,7 @@ import ViewMore from "./components/viewMore/viewMore";
 
 function App() {
   return (
-    <div className="App">
-      <Hotelview />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hi Doris, you are a seniore dev
-        </a>
-      </header> */}
-    </div>
+    
     <BrowserRouter>
     
       <Routes>
