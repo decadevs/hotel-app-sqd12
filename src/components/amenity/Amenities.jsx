675bf072ab0463a84a4fdabec302c71702f694af
victorpraiz <ivictorpraiz@gmail.com>
2023-01-26 16:28:58 +0100

    amenities

--- src/components/amenity/Amenities.jsx
+++ src/components/amenity/Amenities.jsx
@@ -3,9 +3,19 @@ import React from "react";
 import { BiFilter } from "react-icons/bi";
 import { AiOutlinePlus } from "react-icons/ai";
 import { FaSearch } from "react-icons/fa";
-import { MdMoreHoriz, MdOutlineModeEditOutline, MdOutlineDeleteForever } from "react-icons/md";
+import {
+  MdMoreHoriz,
+  MdOutlineModeEditOutline,
+  MdOutlineDeleteForever,
+} from "react-icons/md";
+import Modal from "@mui/material/Modal";
+import { red } from "@mui/material/colors";
 
 export default function () {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <div className="container">
       <h1 className="amenities-h1">Amenities</h1>
@@ -22,7 +32,7 @@ export default function () {
           </div>
         </div>
 
-        <button className="create-amenity-button">
+        <button className="create-amenity-button" onClick={() => handleOpen()}>
           <AiOutlinePlus style={{ marginRight: ".4rem", fontSize: "1rem" }} />
           Create Amenity
         </button>
@@ -36,7 +46,7 @@ export default function () {
           <th>Price</th>
           <th>Discount</th>
           <th>
-            <MdMoreHoriz className="table-icons table-icons-hover"  />
+            <MdMoreHoriz className="table-icons table-icons-hover" />
           </th>
         </tr>
         <tr>
@@ -53,6 +63,54 @@ export default function () {
           </td>
         </tr>
       </table>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          overflowY: "auto",
+          outline: "none",
+          ":focus": {
+            outline: "none",
+            border: "none",
+          },
+        }}
+      >
+        <div className="formBody">
+          <h1 className="formName">Add Amenity</h1>
+          <form>
+            <div>
+              <input
+                type="text"
+                placeholder="Name"
+                className="amenityInput"
+              ></input>
+            </div>
+            <div>
+              <input
+                type="number"
+                placeholder="Price"
+                className="amenityInput"
+              ></input>
+            </div>
+            <div>
+              <input
+                type="number"
+                placeholder="Discount"
+                className="amenityInput"
+              ></input>
+            </div>
+            <button className="modal-button" type="submit">
+              Add Amenity
+            </button>
+          </form>
+        </div>
+      </Modal>
     </div>
   );
 }
--- src/components/amenity/CreateAmenity.jsx
+++ src/components/amenity/CreateAmenity.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from "react";
+import "./CreateAmenity.css";
 
-export default function CreateAmenity() {
+export default function Amenity() {
   return (
-    <div>CreateAmenity</div>
-  )
+    <div className="container">
+      <div className="formBody">
+        <h1 className="formName">Add Amenity</h1>
+        <form>
+          <div>
+            <input
+              type="text"
+              placeholder="Name"
+              className="amenityInput"
+            ></input>
+          </div>
+          <div>
+            <input
+              type="number"
+              placeholder="Price"
+              className="amenityInput"
+            ></input>
+          </div>
+          <div>
+            <input
+              type="number"
+              placeholder="Discount"
+              className="amenityInput"
+            ></input>
+          </div>
+          <button type="submit" className="modal-button">
+            Add Amenity
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
