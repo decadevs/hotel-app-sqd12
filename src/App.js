342aec5d453caf9bd8a139d0cbcee89e1123535a
E-kenny <57300188+E-kenny@users.noreply.github.com>
2023-01-29 23:13:18 +0100

    Dashboard (#37)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-//import "./App.css";
+import CustomerDashboard from "./components/customerHome/Dashboard";
+import {Dashboard} from "./components/ManagerHome/ManagerDashboard";
+import AdminDashboard from "./components/adminHome/Dashboard";
+import CustomerHotel from "./components/customerHome/Hotels";
+import CustomerManager from "./components/customerHome/Manager";
+import CustomerTransaction from "./components/customerHome/Transactions";
+import ManagerHotel from "./components/ManagerHome/Hotels";
+import ManagerBookings from "./components/ManagerHome/Bookings";
+import ManagerTransactions from "./components/ManagerHome/Transactions";
+import AdminHotel from "./components/adminHome/Hotels";
+import AdminManager from "./components/adminHome/Manager";
+import AdminTransaction from "./components/adminHome/Transactions";
 import Login from "./components/authentication/Login";
 import Register from "./components/authentication/Register";
 import ResetUpdatePassword from "../src/components/ResetUpdatePassword/ResetUpdatePassword";
@@ -22,10 +33,7 @@ import Success from "./components/modals/SuccessModal";
 import Booking from "./pages/customer/Booking";
 import ViewMore from "./components/viewMore/viewMore";
 import HotelListing from "./components/HotelListing/HotelListing";
-import ManagerDashboard from "./components/ManagerHome/ManagerDashboard";
-import {Dashboard} from "./components/ManagerHome/ManagerDashboard";
-import HotelReview from "./components/HotelReview/HotelReview";
-import ManagerTransactions from "./components/ManagerHome/managertransactions/ManagerTransactions";
+// import HotelReview from "./components/HotelReview/HotelReview";
 
 
 function App() {
@@ -53,8 +61,18 @@ function App() {
         <Route path="success-modal" element={<Success />} />
         <Route path="view-more" element={<ViewMore />} />
         <Route path="hotel-listing" element={<HotelListing />} />
-        <Route path="managerdashboard" element={<ManagerDashboard />} />
         <Route path="manager-dashboard" element={<Dashboard />} />
+        <Route path="customer-dashboard" element={<CustomerDashboard />} />
+        <Route path="admin-dashboard" element={<AdminDashboard />} />
+        <Route path="manager-transaction" element={<ManagerTransactions />} />
+        <Route path="manager-Bookings" element={<ManagerBookings />} />
+        <Route path="manager-hotel" element={<ManagerHotel />} />
+        <Route path="customer-transaction" element={<CustomerTransaction />} />
+        <Route path="customer-manager" element={<CustomerManager />} />
+        <Route path="customer-hotel" element={<CustomerHotel />} />
+        <Route path="admin-transaction" element={<AdminTransaction />} />
+        <Route path="admin-manager" element={<AdminManager />} />
+        <Route path="admin-hotel" element={<AdminHotel />} />
         <Route path="manager-transactions" element={<ManagerTransactions />} />
         <Route path="amenities" element={<Amenities />} />
       </Routes>
