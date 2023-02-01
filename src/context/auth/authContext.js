5574e4cdcf8bb57549f7eb1df36cab49ba916555
sam-adroit <66552813+sam-adroit@users.noreply.github.com>
2023-02-01 23:23:54 +0100

    Authentication (#45)
    * Add changes
    
    * Added react-router-dom paackage for routing, authentication interface (Register and Login)
    
    * moved all index app to app level and routed
    
    * restructured the project file and moved changePassword, ResetPassword files to components
    
    * added other routing
    
    * Reigstration and Login api integration
    
    * Completed login with saving tokens to loaclstorage
    
    * Added favicon
    
    * Change password and forgot password

--- src/context/auth/authContext.js
+++ src/context/auth/authContext.js
@@ -10,19 +10,42 @@ const AuthContextProvider = (props) => {
   };
   const login = async (value) => {
     const { data } = await apiPost("Authentication/Login", value, {}, false);
-    console.log(data);
     if (data.succeeded) {
       localStorage.setItem("token", data.data);
     }
     return data;
   };
-
+  const forgotPassword = async (email) => {
+    let dataObj = { email };
+    const { data } = await apiPost(
+      "Authentication/Reset-Password",
+      dataObj,
+      {},
+      false
+    );
+    return data;
+  };
+  //
+  const resetPassword = async (value) => {
+    const { data } = await apiPost(
+      "Authentication/Reset-Update-Password",
+      value,
+      {},
+      false
+    );
+    return data;
+  };
+  const changePassword = async (value) => {
+    const { data } = await apiPost("Authentication/Change-Password", value, {});
+    return data;
+  };
   return (
-    <AuthContext.Provider value={{ register, login }}>
+    <AuthContext.Provider
+      value={{ register, login, forgotPassword, resetPassword, changePassword }}
+    >
       {props.children}
     </AuthContext.Provider>
   );
 };
 
 export default AuthContextProvider;
-//newfile
