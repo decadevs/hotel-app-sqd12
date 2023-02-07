1a4b996217b32d635c976a7fee98e6a20033babc
Ekene Obiasogu <ekennyobiasogu@gmail.com>
2023-02-07 11:02:45 +0100

    Setup

--- src/components/navs/Navbar.js
+++ src/components/navs/Navbar.js
@@ -27,10 +27,12 @@ export default function Navbar() {
                     <div>
                         <input type="text"/>
                     </div>
+
                     {/* ADD MANAGER REQUEST */}
                     <div>
                         <ManagerRequest/>
                     </div>
+                    
                     <div className="right-nav">
                         <div className="bell">
                             <img src={Bell} alt="Your SVG" />
